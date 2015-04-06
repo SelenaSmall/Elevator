010860f6516268f39a4000012dee35992589c5f9
SelenaSmall <selenawiththetattoo@gmail.com>
2015-04-07 00:05:52 +1200

    elevator

---
+++ main.rb
@@ -0,0 +1,29 @@
+##Pseudo elevator moving in two directions for an apartment 
+##building with 7 floors
+
+
+#Does lift exist?
+#Where is lift currently?
+
+#if lift has nothing to do, return to level 1
+
+#lift ascend to highest level called (not above level 7)
+#stop at all levels which are higher than the current level AND that
+#have called to go to a higher level. ALSO
+#stop at all stops which are higher than the current level AND that
+#have been called AND that are higher than the level which first 
+#called them
+
+#if no level (greater than the current level) has called to go to 
+#a higher level then{
+
+	
+#lift descend to lowest level called (not below 1)
+#stop at all levels which are lower than the current level AND that
+#have called to go to a lower level. ALSO
+#stop at all stops which are lower than the current level AND that 
+#have been called AND that are lower than the level which first 
+#called them}
+
+###this is all good in theory but its not until the user is actually 
+###in the lift that they decide where they want to go to ;p 
