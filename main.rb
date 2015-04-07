cc0f2f5aa58cdb6d7d8289b32c9b12bb3e33fb04
SelenaSmall <selenawiththetattoo@gmail.com>
2015-04-08 00:43:12 +1200

    more development on how the elevator actually might work

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@
 
 ##Pseudo elevator moving in two directions for an apartment building with 7 floors
 
+=begin 
 class Elevator
 
 	def initialize(call, move)
@@ -17,13 +18,67 @@ elevator=Elevator.new("Called", "Moving")
 
 puts "Lift called: #{elevator.call}"
 puts "Lift moving: #{elevator.move}"
+=end
 
+#The elevator doesn't call the elevator, the floor within a building calls the elevator
+
+class Floor		#has ability to call lift going up or lift going down
+
+	@@no_of_floors=0
+
+	def initialize(floor_number)
+		@floor_number=floor_number
+		@@no_of_floors +=1
+	end
+
+	attr_reader :floor_number
+
+	def call_lift_going_up
+		#TODO: all floors can call lift going up (except the top floor)
+	end
+
+	def call_lift_going_down
+		#TODO: all floors can call lift going down (except the bottom floor)
+	end
+end
+
+class Elevator		##has ability to move up; move down; stop and open
+	
+	def initialize(current_location)
+		@current_location=current_location
+	end
+
+	def move_up
+	end
+
+	def move_down
+	end
+
+	def stop_and_open
+	end
+
+	def stop_and_stay_closed
+	end
+	
+	def move_efficiently
+		#TODO: move down AND stop and open
+		#TODO: ELSE move up AND stop and open
+		#TODO: ELSE stop and stay closed
+	end
+
+end
+
+floor1=Floor.new(1)
+floor2=Floor.new(2)
+floor3=Floor.new(3)
+
+elevator=Elevator.new(1)
+
+puts "Elevators current location is on floor: #{floor1.floor_number}"
 
 
 #Does lift exist?
 #Where is lift currently?
-
-#if lift has nothing to do, return to level 1
 
 #lift ascend to highest level called (not above level 7)
 #stop at all levels which are higher than the current level AND that
@@ -43,5 +98,4 @@ puts "Lift moving: #{elevator.move}"
 #have been called AND that are lower than the level which first 
 #called them}
 
-###this is all good in theory but its not until the user is actually 
-###in the lift that they decide where they want to go to ;p 
+###its not until the user is actually in the lift that they decide where they want to go to ;p 
