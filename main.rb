6811eae6a7e8eccff989dee47ff89a9e211bb3de
SelenaSmall <selenawiththetattoo@gmail.com>
2015-04-09 18:28:53 +1200

    count numbre of floors

--- main.rb
+++ main.rb
@@ -6,27 +6,45 @@
 class Floor		#has ability to call lift going up or lift going down
 
 	@@no_of_floors=0
+	@@store_floor_number={}
 
 	def initialize(floor_number)
 		@floor_number=floor_number
 		@@no_of_floors +=1
+		@@store_floor_number[@floor_number] ||= 0		
+		@@store_floor_number[@floor_number] += 1
 	end
 
-	attr_reader :floor_number
+	#attr_reader :floor_number
+
+	def total_number_of_floors
+		@@no_of_floors
+	end
+
+	def which_floor_is_calling_the_lift
+#TODO: determine which floor is calling the lift
+	end
 
 	def call_lift_going_up
-		puts "Press 1 to call the lift\n"
+##TODO: optimize to call_lift with 3 case statements: up, down, exit
+		puts "Press 1 to call the lift going up\n"
 		number = gets.chomp.to_i
 
 		if number.to_i == 1
 			puts "You have called the lift going up"
-		else puts "FAIL"
+		else puts "FAIL" 
 		end
 		#TODO: all floors can call lift going up (except the top floor)
 	end
 
 	def call_lift_going_down
-		#TODO: #2 on user keyboard calls lift going down
+		puts "Press 2 to call the lift going down\n"
+		number = gets.chomp.to_i
+
+		if number.to_i == 2
+			puts "You have called the lift going up"
+		else puts "FAIL" 
+		end
 		#TODO: all floors can call lift going down (except the bottom floor)
 	end
 end
@@ -64,6 +82,8 @@ floor3=Floor.new(3)
 elevator=Elevator.new(1)
 
 #puts "Elevators current location is on floor: #{floor1.floor_number}"
+puts "Number of floors: #{floor1.total_number_of_floors}"
+puts "Your are on floor x"
 floor1.call_lift_going_up
 
 
